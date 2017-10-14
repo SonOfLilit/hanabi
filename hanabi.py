fa9248f20beced8939ce3e2e4d66e6b38ae8eb4d
tsvikas <tsvikas@gmail.com>
2017-10-14 23:24:30 +0300

    minor main improvements

--- hanabi.py
+++ hanabi.py
@@ -11,7 +11,7 @@ def run_game_n_times(player, t, num_players=3, end_mode=EndMode.official, suits=
     import pandas as pd
     d = pd.Series(score)
     print(d.describe())
-    print(d.value_counts(sort=False))
+    print(d.value_counts().sort_index())
     return d
 
 
@@ -40,4 +40,4 @@ def main():
         return run_game_once(player, *h_args)
 
 if __name__ == '__main__':
-    main()
+    ret = main()
